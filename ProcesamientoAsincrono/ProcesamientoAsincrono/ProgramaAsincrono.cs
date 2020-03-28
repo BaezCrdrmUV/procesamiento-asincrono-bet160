fb1cb2e254e1d6e3e05e3aac61bd14921286713d
bet160 <36347319+bet160@users.noreply.github.com>
2020-03-27 23:16:49 -0600

    Proyecto de visual studio
    Ejemplo en el lenguaje de programación C#

---
+++ ProcesamientoAsincrono/ProcesamientoAsincrono/ProgramaAsincrono.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ProcesamientoAsincrono
+{
+    class ProgramaAsincrono
+    {
+        static void Main(string[] args)
+        {
+            var porcentaje = CalcularPorcentaje(70);
+            Metodo1();
+
+            porcentaje.Wait();
+            Console.ReadLine();
+        }
+
+        private static void Metodo1()
+        {
+            Console.WriteLine("Ejecucion asincrona del metodo 1");
+        }
+
+        static async Task<float> CalcularPorcentaje(float value)
+        {
+            Console.WriteLine("Tarea de calcular el 50% de una cantidad ejecutandose de manera asincrona");
+
+            var resultado = await Task.Run(() => value * .5f);
+
+            Console.WriteLine($"Tarea terminada. El 50% de {value} es: {resultado} ");
+
+            return resultado;
+        }
+    }
+}
